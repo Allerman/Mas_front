0fd4de5ae424d34b45ec6a998d72bf2b343084cc
allerman <allerman3301@gmail.com>
2023-12-05 13:56:40 -0300

    refazendo map

---
+++ src/components/Mapa.js
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+
+function Mapa() {
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = parseFloat(position.coords.latitude);
+          const longitude = parseFloat(position.coords.longitude);
+
+          const mapOptions = {
+            center: { lat: latitude, lng: longitude },
+            zoom: 14,
+          };
+
+          const map = new window.google.maps.Map(
+            document.getElementById('map'),
+            mapOptions
+          );
+
+          const userMarker = new window.google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            title: 'Sua Localização Atual',
+          });
+
+          carregarDenunciasDoJSON(map);
+        },
+        function (error) {
+          handleLocationError(error);
+        }
+      );
+    } else {
+      alert('A geolocalização não é suportada neste navegador.');
+    }
+  }, []); // Executar apenas uma vez ao montar o componente
+
+  function initMap(latitude, longitude) {
+    if (
+      typeof latitude !== 'number' ||
+      isNaN(latitude) ||
+      typeof longitude !== 'number' ||
+      isNaN(longitude)
+    ) {
+      return;
+    }
+
+    const mapDiv = document.getElementById('map');
+    const mapOptions = {
+      center: { lat: latitude, lng: longitude },
+      zoom: 14,
+    };
+    const map = new window.google.maps.Map(mapDiv, mapOptions);
+
+    const marker = new window.google.maps.Marker({
+      position: { lat: latitude, lng: longitude },
+      map: map,
+      title: 'Sua Localização Atual',
+    });
+  }
+
+  function handleLocationError(error) {
+    switch (error.code) {
+      case 1:
+        alert('Permissão de localização negada. Por favor, habilite a localização.');
+        break;
+      case 2:
+        alert('Localização não disponível no momento. Tente novamente mais tarde.');
+        break;
+      case 3:
+        alert('Tempo limite da solicitação de localização expirado.');
+        break;
+      default:
+        alert('Ocorreu um erro desconhecido ao obter a localização.');
+    }
+  }
+
+  function adicionarMarcadoresNoMapa(map, denuncias) {
+    denuncias.forEach((denuncia) => {
+      const latitude = parseFloat(denuncia.latitude);
+      const longitude = parseFloat(denuncia.longitude);
+      const textoDenuncia = denuncia.textoDenuncia;
+
+      const customIcon = {
+        url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+        scaledSize: new window.google.maps.Size(30, 30),
+      };
+
+      const marker = new window.google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map,
+        title: textoDenuncia,
+        icon: customIcon,
+      });
+    });
+  }
+
+  function carregarDenunciasDoJSON(map) {
+    fetch('assets/json/denuncia.json')
+      .then((response) => response.json())
+      .then((data) => {
+        adicionarMarcadoresNoMapa(map, data.denuncia);
+      })
+      .catch((error) => {
+        console.error('Erro ao carregar o arquivo JSON:', error);
+      });
+  }
+
+  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+}
+
+export default Mapa;
